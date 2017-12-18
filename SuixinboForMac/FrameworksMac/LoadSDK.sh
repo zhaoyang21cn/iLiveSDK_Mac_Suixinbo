5d1f98e7506698e97a42fdaf0bb78c31cd0d60ad
wilderliao <wilderliao@tencent.com>
2017-12-18 11:40:20 +0800

    更新1.8.1版本

--- SuixinboForMac/FrameworksMac/LoadSDK.sh
+++ SuixinboForMac/FrameworksMac/LoadSDK.sh
@@ -3,9 +3,10 @@
 
 # 加载各个模块SDK
 echo "|-------------------------------sdk version--------------------------------|"
-ILiveSDKVersion="ILiveSDKMac_1.8.0.12629"
+ILiveSDKVersion="ILiveSDKMac_1.8.1.12629"
 IMSDKSDKVersion="IMSDK_2.5.4.10421.10420"
-AVSDKVersion="AVSDK_1.9.6.1204.Local"
+AVSDKVersion="AVSDK_1.9.6.1212.Local"
+
 
 echo $ILiveSDKVersion
 echo $IMSDKSDKVersion
