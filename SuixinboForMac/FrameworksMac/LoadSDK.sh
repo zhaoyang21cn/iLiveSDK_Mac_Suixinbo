b515c3e3877675edd742f4e718e0efaf31b23cd4
wilderliao <wilderliao@wilderliao-mc0.tencent.com>
2018-07-09 15:12:30 +0800

    1.9.0， 默认使用云上环境

--- SuixinboForMac/FrameworksMac/LoadSDK.sh
+++ SuixinboForMac/FrameworksMac/LoadSDK.sh
@@ -3,9 +3,9 @@
 
 # 加载各个模块SDK
 echo "|-------------------------------sdk version--------------------------------|"
-ILiveSDKVersion="ILiveSDKMac_1.8.4.13487"
+ILiveSDKVersion="ILiveSDKMac_1.9.0.13639"
 IMSDKSDKVersion="IMSDK_2.5.4.10421.10420"
-AVSDKVersion="AVSDK_1.9.8.5.Local"
+AVSDKVersion="AVSDK_1.9.8.29.Local"
 
 
 echo $ILiveSDKVersion
