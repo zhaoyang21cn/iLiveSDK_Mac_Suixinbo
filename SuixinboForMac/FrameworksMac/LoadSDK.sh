f859866da1e46bbbc51f770831eb3579cea754a8
iliveSDK <githubzt@gmail.com>
2018-09-05 11:09:12 +0800

    Update LoadSDK.sh

--- SuixinboForMac/FrameworksMac/LoadSDK.sh
+++ SuixinboForMac/FrameworksMac/LoadSDK.sh
@@ -3,7 +3,7 @@
 
 # 加载各个模块SDK
 echo "|-------------------------------sdk version--------------------------------|"
-ILiveSDKVersion="ILiveSDKMac_1.9.4.14015"
+ILiveSDKVersion="ILiveSDKMac_1.9.4.14018"
 IMSDKSDKVersion="IMSDK_2.5.4.10421.10420"
 AVSDKVersion="AVSDK_1.9.9.1012.39687"
 
